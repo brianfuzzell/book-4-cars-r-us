05c7231dbb01e6cc64abd1a19ad1d597bd1099aa
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-03-12 11:43:28 -0500

    Flesh out wheel options

---
+++ scripts/wheelsOptions.js
@@ -0,0 +1,38 @@
+// import from transientState.js
+
+// Set up change handler
+const handleWheelSelection = (changeEvent) => {
+    if (changeEvent.target.id === "wheel-options") {
+        const chosenOption = changeEvent.target.value
+        console.log(parseInt(chosenOption))
+    }
+}
+
+export const wheelOptions = async () => {
+    // Fetch wheels from the API
+
+    // console.log(wheels)
+
+    // Add an event listener
+    document.addEventListener("change", handleWheelSelection)
+
+    let optionsHTML = ""
+
+    // Generate a new array of HTML strings using the Array.map() method
+    const divStringArray = wheels.map(
+        (wheel) => {
+            return `<div>
+                <select id="wheel-options">
+                    <option value="0">Select your wheels</option>
+                    <option value="${wheel.id}"> ${wheel.option}</option>
+                </select>
+            </div>`
+        }
+    )
+
+    // console.log(divStringArray)
+
+    optionsHTML += divStringArray.join("")
+
+    return optionsHTML
+}
