c17a5be76255fd0b6d9e0ed9d52b03b5d501c349
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-05-19 10:59:09 -0500

    Refactor calculating total price

--- scripts/ordersList.js
+++ scripts/ordersList.js
@@ -1,36 +1,27 @@
 export const ordersList = async () => {
-    // Fetch orders from the API
-    const response = await fetch("https://localhost:7204/orders")
-    const orders = await response.json()
+  // Fetch orders from the API
+  const response = await fetch("https://localhost:7204/orders");
+  const orders = await response.json();
 
-    // Generate the HTML representation for each order
-    let html = `
+  // Generate the HTML representation for each order
+  let html = `
         <div id="order-submissions">
             <h2>Custom Car Orders</h2>
-    `
+    `;
 
-    const ordersHTML = orders.map(
-        (order) => {
-            const orderPrice = order.paintColor.price + order.interior.price + order.technology.price + order.wheels.price
-            // Trying out the toLocalString() method to format the order price
-            const formattedPrice = orderPrice.toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD"
-            })
-
-            return `
+  const ordersHTML = orders.map((order) => {
+    return `
                 <section class="order-submission-container">
-                    <p>${order.paintColor.color} car with ${order.wheels.style} wheels, ${order.interior.material} interior, and the ${order.technology.package} for a total cost of ${formattedPrice}</p>
+                    <p>${order.paintColor.color} car with ${order.wheels.style} wheels, ${order.interior.material} interior, and the ${order.technology.package} for a total cost of ${order.totalCost.toLocaleString("en-US", { style: "currency", currency: "USD" })}</p>
                 </section>
-            `
-        }
-    )
+            `;
+  });
 
-    html += ordersHTML.join("")
+  html += ordersHTML.join("");
 
-    html += `
+  html += `
         </div>
-    `
+    `;
 
-    return html
-}
+  return html;
+};
